25e6a488559b865619ab3adb8b41495b41c82d9b
srnpr <srnprzjwc@srnpr.com>
2013-08-15 09:52:16 +0800

    a

--- zapweb/src/main/java/com/srnpr/zapweb/webfunc/FuncRefreshCache.java
+++ zapweb/src/main/java/com/srnpr/zapweb/webfunc/FuncRefreshCache.java
@@ -4,8 +4,16 @@ import com.srnpr.zapcom.basemodel.MDataMap;
 import com.srnpr.zapweb.topcall.InitZapweb;
 import com.srnpr.zapweb.webmodel.MWebResult;
 
+/**
+ * 刷新系统缓存按钮
+ * @author srnpr
+ *
+ */
 public class FuncRefreshCache extends RootFunc {
 
+	/* (non-Javadoc)
+	 * @see com.srnpr.zapweb.webface.IWebFunc#funcDo(java.lang.String, com.srnpr.zapcom.basemodel.MDataMap)
+	 */
 	public MWebResult funcDo(String sOperateUid, MDataMap mDataMap) {
 		
 		
