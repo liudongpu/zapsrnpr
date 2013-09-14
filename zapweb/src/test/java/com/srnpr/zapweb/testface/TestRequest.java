0973a441e8fb7e3763713b2d567a32e1f7245548
srnpr <srnpr@srnpr.com>
2013-09-14 19:22:14 +0800

    a

--- zapweb/src/test/java/com/srnpr/zapweb/testface/TestRequest.java
+++ zapweb/src/test/java/com/srnpr/zapweb/testface/TestRequest.java
@@ -14,76 +14,9 @@ import com.srnpr.zapweb.webface.IWebResult;
 
 public class TestRequest extends TestHelper {
 
-	@Test
+	
 	public void test() {
 
-		try {
-
-			String sTextString = "";
-
-			Class<?> c = Class.forName("com.srnpr.zapweb.testface.TestFace1");
-
-			// c.getMethods()[0].
-			Class<?> cReturn = null;
-			Class<?> cInputClass = null;
-			for (Method method : c.getMethods()) {
-
-				if (method.getName().equals("DoProcess") && !method.isBridge()) {
-					cReturn = method.getReturnType();
-					cInputClass = method.getParameterTypes()[0];
-				
-				}
-
-			}
-
-			TestInput tInput = new TestInput();
-			tInput.setField1("abc");
-
-			JsonHelper<TestInput> tHelper = new JsonHelper<TestInput>();
-
-			sTextString = tHelper.ObjToString(tInput);
-
-			bLogTest(sTextString);
-
-			IWebInput iInput = (IWebInput) cInputClass.newInstance();
-
-			JsonHelper<IWebInput> oHelper = new JsonHelper<IWebInput>();
-
-			iInput = oHelper.StringToObj(sTextString, iInput);
-
-			sTextString = tHelper.ObjToString(tInput);
-
-			bLogTest(sTextString);
-
-			IWebFace tFace1 = (IWebFace) c.newInstance();
-
-			IWebResult iResult = (IWebResult) tFace1.DoProcess(iInput);
-
-			JsonHelper<IWebResult> jsonResult = new JsonHelper<IWebResult>();
-
-			sTextString = jsonResult.ObjToString(iResult);
-
-			bLogTest(sTextString);
-
-			/*
-			 * Object object = new Object();
-			 * 
-			 * JsonHelper<Object> oHelper = new JsonHelper<Object>();
-			 * 
-			 * oHelper.StringToObj(sTextString, object);
-			 * 
-			 * IWebFace tFace1 = new TestFace1();
-			 * 
-			 * tFace1.Call(object);
-			 */
-			// IWebFace tFace1=new TestFace1();
-
-			// tFace1.Call()
-
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		}
 
 	}
 
