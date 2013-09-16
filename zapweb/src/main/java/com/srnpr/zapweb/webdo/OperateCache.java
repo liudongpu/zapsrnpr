5748a6c71f92df732fcd82e7ea6c7e81be58d431
srnpr <srnpr@srnpr.com>
2013-09-16 21:13:52 +0800

    a

--- zapweb/src/main/java/com/srnpr/zapweb/webdo/OperateCache.java
+++ zapweb/src/main/java/com/srnpr/zapweb/webdo/OperateCache.java
@@ -10,8 +10,19 @@ import com.srnpr.zapdata.dbdo.DbUp;
 import com.srnpr.zapweb.webface.IWebFunc;
 import com.srnpr.zapweb.webmodel.MWebOperate;
 
+/**
+ * 操作按钮缓存
+ * 
+ * @author srnpr
+ * 
+ */
 public class OperateCache extends RootCache<String, MWebOperate> {
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.srnpr.zapcom.baseface.IBaseCache#refresh()
+	 */
 	public synchronized void refresh() {
 
 		for (MDataMap mDataMap : DbUp.upTable("zw_operate").queryByWhere(
