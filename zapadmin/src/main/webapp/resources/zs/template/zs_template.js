466d0370cd15c581e976d51cee37fac10a7979d6
srnpr <srnpr@srnpr.com>
2013-10-14 15:37:46 +0800

    a

---
+++ zapadmin/src/main/webapp/resources/zs/template/zs_template.js
@@ -0,0 +1,22 @@
+
+var zs_template=
+{
+	
+	
+	
+};
+
+
+
+
+
+
+if ( typeof define === "function" && define.amd) {
+	define("template/zs_template", function() {
+		return zs_template;
+	});
+}
+
+
+
+
