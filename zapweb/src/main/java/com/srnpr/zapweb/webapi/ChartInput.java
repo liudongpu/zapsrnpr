c97be11808c312ba6eb36e7cfbbe1d0adf360f5a
srnpr <srnpr@srnpr.com>
2013-10-23 18:30:00 +0800

    a

---
+++ zapweb/src/main/java/com/srnpr/zapweb/webapi/ChartInput.java
@@ -0,0 +1,22 @@
+package com.srnpr.zapweb.webapi;
+
+import com.srnpr.zapcom.topapi.RootInput;
+
+public class ChartInput extends RootInput {
+
+	
+	/**
+	 * 页面编号
+	 */
+	private String pageCode="";
+
+	public String getPageCode() {
+		return pageCode;
+	}
+
+	public void setPageCode(String pageCode) {
+		this.pageCode = pageCode;
+	}
+	
+	
+}
