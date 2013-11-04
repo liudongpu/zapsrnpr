321da5580528db36d52bf3430e5086536eb31b1c
srnpr <srnpr@srnpr.com>
2013-11-04 17:38:58 +0800

    a

--- zapcom/src/main/java/com/srnpr/zapcom/baseface/IBaseInit.java
+++ zapcom/src/main/java/com/srnpr/zapcom/baseface/IBaseInit.java
@@ -1,10 +1,15 @@
 package com.srnpr.zapcom.baseface;
 
 
+/**
+ * 基本初始化
+ * @author srnpr
+ *
+ */
 public interface IBaseInit {
 
 	/**
-	 * 初始化
+	 * 初始化  返回是否初始化正确
 	 */
 	public boolean init();
 }
--- zapzero/src/main/java/com/srnpr/zapzero/topcall/InitZapzero.java
+++ zapzero/src/main/java/com/srnpr/zapzero/topcall/InitZapzero.java
@@ -8,15 +8,21 @@ import com.srnpr.zapcom.basehelper.IoHelper;
 import com.srnpr.zapcom.rootclass.RootInit;
 import com.srnpr.zapcom.topdo.TopDir;
 
-public class InitZapzero extends BaseClass implements IBaseInit {
+public class InitZapzero extends RootInit {
 
 	public boolean init() {
 		return initServer();
 	}
 
+	/**
+	 * 初始化运行模式
+	 * @return
+	 */
 	private boolean initServer() {
 
 		bLogInfo(970212010, bConfig("default.local_server_type"));
+		bLogInfo(970212011, bConfig("default.leader_server_address"));
+		
 
 		return true;
 
