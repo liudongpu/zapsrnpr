cb6acba4c1ed26491ecf347e8d9787d7985d53c6
srnpr <srnpr@srnprmacmini>
2014-03-02 18:15:18 +0800

    a

--- zapweb/src/main/java/com/srnpr/zapweb/webclass/SysInfo.java
+++ zapweb/src/main/java/com/srnpr/zapweb/webclass/SysInfo.java
@@ -4,6 +4,10 @@ import java.util.Enumeration;
 import java.util.Map;
 import java.util.Properties;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.StringUtils;
+
 import com.srnpr.zapcom.baseclass.BaseClass;
 import com.srnpr.zapcom.basehelper.NetHelper;
 import com.srnpr.zapcom.basemodel.MDataMap;
@@ -15,9 +19,32 @@ public class SysInfo extends BaseClass {
 
 		MDataMap mDataMap = new MDataMap();
 
-		mDataMap.put("ip", NetHelper.getLocalIP());
+		mDataMap.put("sysinfo_ip", NetHelper.getLocalIP());
 
 		WebSessionHelper webSessionHelper = WebSessionHelper.create();
+
+		HttpServletRequest request = webSessionHelper.upHttpRequest();
+
+		mDataMap.put("sysinfo_remotehost", webSessionHelper.upHttpRequest()
+				.getRemoteHost()
+				+ ":"
+				+ webSessionHelper.upHttpRequest().getRemotePort());
+
+		mDataMap.put("sysinfo_local", webSessionHelper.upHttpRequest()
+				.getLocalAddr()
+				+ ":"
+				+ webSessionHelper.upHttpRequest().getLocalPort());
+		mDataMap.put("sysinfo_requesturi",
+
+		"http://" + request.getServerName() // 服务器地址
+				+ ":" + request.getServerPort() // 端口号
+				+ request.getContextPath() // 项目名称
+				+ request.getServletPath() // 请求页面或其他地址
+
+				+ (StringUtils.isBlank(request.getQueryString()) ? ""
+				: ("?" + request.getQueryString()))
+
+		);
 
 		if (webSessionHelper.upRequest("t").equals("all")) {
 
