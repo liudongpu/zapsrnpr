03e6e66254fcf58ab785f529d0721bac89afd3b8
srnpr <srnpr@srnpr.com>
2014-03-06 18:19:28 +0800

    a

--- zapcom/src/main/java/com/srnpr/zapcom/basehelper/BeansHelper.java
+++ zapcom/src/main/java/com/srnpr/zapcom/basehelper/BeansHelper.java
@@ -16,10 +16,18 @@ public class BeansHelper implements IBaseHelper {
 
 	private static BeanFactory beanFactory = null;
 
+	private static int flagInit = 0;
+
 	private static Object getBeanObject(String name) {
 
-		if (beanFactory == null) {
-			initBeanFactory();
+		if (flagInit < 1 || beanFactory == null) {
+			flagInit = 2;
+			if (flagInit == 2) {
+				flagInit = 3;
+				if (beanFactory == null) {
+					new BeansHelper().initBeanFactory();
+				}
+			}
 		}
 
 		return beanFactory.getBean(name);
@@ -30,14 +38,13 @@ public class BeansHelper implements IBaseHelper {
 		return (T) getBeanObject(sBeanName);
 	}
 
-	private synchronized static void initBeanFactory() {
+	private synchronized void initBeanFactory() {
 
 		if (beanFactory == null) {
 			String[] sSpringConfig = TopUp.upConfig("zapcom.spring_bean")
 					.split(",");
 
-			BaseLog.LogInfo(new BeansHelper().getClass().getName(), 0,
-					sSpringConfig);
+			BaseLog.LogInfo(this.getClass().getName(), 0, sSpringConfig);
 
 			beanFactory = new ClassPathXmlApplicationContext(sSpringConfig)
 					.getBeanFactory();
