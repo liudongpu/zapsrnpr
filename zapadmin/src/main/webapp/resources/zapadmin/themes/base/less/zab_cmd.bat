c00d1a1207a2b69080b22961418ed666ab707708
srnpr <srnpr@srnpr.com>
2014-04-15 15:03:32 +0800

    a

--- zapadmin/src/main/webapp/resources/zapadmin/themes/base/less/zab_cmd.bat
+++ zapadmin/src/main/webapp/resources/zapadmin/themes/base/less/zab_cmd.bat
@@ -2,5 +2,6 @@
 start zab_cmd_run.bat zab_less_green.less  ../../green/css/zab_css.css
 start zab_cmd_run.bat zab_less_orange.less ../../orange/css/zab_css.css
 start zab_cmd_run.bat zab_less_blue.less   ../../blue/css/zab_css.css
+start zab_cmd_run.bat zab_less_cyan.less   ../../cyan/css/zab_css.css
 
 
